645bae4d8950790f17a931996d3f971e8664a803
Brennan Kastner <brennan@prestigepromedia.com>
2020-07-21 17:41:14 -0500

    Fix crash caused by attempting to wrap a null input connection (#2)
    * fix: don't attempt to wrap input collection if the super call returned null & update the requestPermission version check to match the open PR this repo is based on.
    
    * Apply suggestions from code review
    
    Co-authored-by: Gustavo Parreira <gustavotcparreira@gmail.com>
    
    * refactor: remove unused import and revert a formatting change
    
    Co-authored-by: Gustavo Parreira <gustavotcparreira@gmail.com>

--- android/src/main/java/com/rnimagekeyboard/ReactMediaInputManager.java
+++ android/src/main/java/com/rnimagekeyboard/ReactMediaInputManager.java
@@ -3,7 +3,6 @@ package com.rnimagekeyboard;
 import android.content.ClipDescription;
 import android.content.Context;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Base64OutputStream;
@@ -12,6 +11,7 @@ import android.view.inputmethod.InputConnection;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.os.BuildCompat;
 import androidx.core.view.inputmethod.EditorInfoCompat;
 import androidx.core.view.inputmethod.InputConnectionCompat;
 import androidx.core.view.inputmethod.InputContentInfoCompat;
@@ -138,8 +138,8 @@ public class ReactMediaInputManager extends ReactTextInputManager {
                             public boolean onCommitContent(InputContentInfoCompat inputContentInfo,
                                                            int flags, Bundle opts) {
                                 // read and display inputContentInfo asynchronously
-                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1 && (flags &
-                                        InputConnectionCompat.INPUT_CONTENT_GRANT_READ_URI_PERMISSION) != 0) {
+                                if (BuildCompat.isAtLeastNMR1() &&
+                                    (flags & InputConnectionCompat.INPUT_CONTENT_GRANT_READ_URI_PERMISSION) != 0) {
                                     try {
                                         inputContentInfo.requestPermission();
                                     }
@@ -190,7 +190,8 @@ public class ReactMediaInputManager extends ReactTextInputManager {
                             }
                         };
 
-                return InputConnectionCompat.createWrapper(ic, outAttrs, callback);
+                // Only wrap non-null input connections otherwise a crash will occur
+                return ic != null ? InputConnectionCompat.createWrapper(ic, outAttrs, callback) : ic;
             }
         };
     }
